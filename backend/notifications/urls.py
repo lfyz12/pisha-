d6b6a9757791b5e2bd5defddf91c311c7040938d
JustErrore40 <f9d9e8@gmail.com>
2026-07-12 13:06:47 +0300

    fix(notifications): add trailing slashes to url routes

--- backend/notifications/urls.py
+++ backend/notifications/urls.py
@@ -4,6 +4,6 @@ from . import views
 
 urlpatterns = [
     path("", views.notification_list_view, name="notifications"),
-    path("<str:pk>/read", views.mark_read_view, name="mark-read"),
-    path("mark-all-read", views.mark_all_read_view, name="mark-all-read"),
+    path("<str:pk>/read/", views.mark_read_view, name="mark-read"),
+    path("mark-all-read/", views.mark_all_read_view, name="mark-all-read"),
 ]
