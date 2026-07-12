09e8543c3fcd18e1dc2a3770d1dc94def979311b
JustErrore40 <f9d9e8@gmail.com>
2026-07-12 10:03:02 +0300

    fix(students): restrict write actions to admins

--- backend/students/views.py
+++ backend/students/views.py
@@ -39,7 +39,7 @@ class StudentViewSet(viewsets.ModelViewSet):
         return qs
 
     def get_permissions(self):
-        if self.action in ["update", "partial_update"]:
+        if self.action in ["create", "update", "partial_update", "destroy"]:
             return [IsAdminUser()]
         return [IsAuthenticated()]
 
